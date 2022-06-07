6844c8db92d1eda5daa8701acddc3b8fcc5487b5
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-07 15:43:02 +0900

    added basic json

--- app.js
+++ app.js
@@ -5,7 +5,41 @@ const { products } = require("./data");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home page</h1><a href="/api/products">products</a>');
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("<h1>Error 404. Product does not exist</h1>");
+  }
+
+  return res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matched your search");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
