148128e507933fcf49bc610f29979f53ad8c81b7
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-09 14:05:04 +0900

    another way of setting routes

--- routes/people.js
+++ routes/people.js
@@ -9,10 +9,16 @@ const {
   deletePerson,
 } = require("../controllers/people");
 
-router.get("/", getPeople);
-router.post("/", createPerson);
-router.post("/postman", createPersonPostman);
-router.put("/:id", updatePerson);
-router.delete("/:id", deletePerson);
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
+
+// This is another way of setting routes.
+// But they both work the same way and functionality is exactly the same as well
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
