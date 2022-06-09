cf8aa4c95ab09ee38890837f7ed890047cbe705b
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-09 10:20:18 +0900

    put method is added

--- app.js
+++ app.js
@@ -41,6 +41,25 @@ app.post("/login", (req, res) => {
   res.status(401).send("Please Provide Credentials");
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id} ` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(3000, () => console.log("Server is running on port 3000..."));
 
 //2-express-tutorial
